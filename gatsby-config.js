025d94fa4b7df87dbaf24a911f5a6246ff9adb45
StellaKim1230 <stellakim1230@gmail.com>
2022-01-09 21:44:27 +0900

    ADD Gatsby install.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `JiEun's Blog`,
     author: {
-      name: `Kyle Mathews`,
+      name: `Kim Ji Eun`,
       summary: `who lives and works in San Francisco building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
