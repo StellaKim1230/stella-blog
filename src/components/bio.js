23254928ca95bb31bb989091411d61ab0ef3b8a2
StellaKim1230 <stellakim1230@gmail.com>
2022-04-03 18:45:47 +0900

    USE My Profile Image.

--- src/components/bio.js
+++ src/components/bio.js
@@ -36,18 +36,18 @@ const Bio = () => {
         className="bio-avatar"
         layout="fixed"
         formats={["auto", "webp", "avif"]}
-        src="../images/profile-pic.png"
+        src="../images/profile.png"
         width={50}
         height={50}
         quality={95}
-        alt="Profile picture"
+        alt="Profile"
       />
       {author?.name && (
         <p>
           Written by <strong>{author.name}</strong> {author?.summary || null}
           {` `}
           <a href={`https://twitter.com/${social?.twitter || ``}`}>
-            You should follow them on Twitter
+            You should follow me on Twitter
           </a>
         </p>
       )}
