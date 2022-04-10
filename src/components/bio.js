1543e13c00bcf3b69993a282e4362c2d9d1ca151
StellaKim1230 <stellakim1230@gmail.com>
2022-04-10 14:20:34 +0900

    Add previos blog content.

--- src/components/bio.js
+++ src/components/bio.js
@@ -36,11 +36,11 @@ const Bio = () => {
         className="bio-avatar"
         layout="fixed"
         formats={["auto", "webp", "avif"]}
-        src="../images/profile.png"
+        src="../images/profile-pic.png"
         width={50}
         height={50}
         quality={95}
-        alt="Profile"
+        alt="Profile Pic"
       />
       {author?.name && (
         <p>
